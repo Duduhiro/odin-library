4b3d597a5cbf1fdd81744ba148fc609bd463ef5d
Duduhiro <duduhiro0312@gmail.com>
2023-10-23 05:00:05 -0300

    Added the feature to input authors name

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ let books = []
 const appendBook = () => {
     let add = true
     const bookName = document.getElementById('bookname').value
+    const authorName = document.getElementById('bookauthor').value
     books.forEach(element => {
-        if (bookName == element) {
+        if (bookName == element.title) {
             alert('Book already added!')
             add = false
         }
     });
     if (add) {
-        books.push(bookName)
-        renderBook(bookName)
+        const book = {
+            'author': authorName,
+            'title': bookName
+        }
+        books.push(book)
+        renderBook(book)
     }
 }
 
-const renderBook = (bookName) => {
+const renderBook = (book) => {
     const shelf = document.getElementById('bookshelf')
-    const book = document.createElement('div')
-    book.setAttribute('class', 'book')
-    book.innerHTML =  bookName
-    shelf.append(book)
+    const newBook = document.createElement('div')
+    const title = document.createElement('h1')
+    const author = document.createElement('h2')
+    title.innerHTML = book.title
+    author.innerHTML = book.author
+    newBook.append(title)
+    newBook.append(author)
+    newBook.setAttribute('class', 'book')
+    shelf.append(newBook)
 }
